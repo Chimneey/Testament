c67a7be149d1660599f783322a62d7163b09b022
Jermaine Watkins <jermainewatkins@gmail.com>
2022-10-09 06:20:25 -0500

    hardhat setting

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -2,5 +2,14 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  defaultNetwork: "matic",
+  networks: {
+    hardhat: {
+    },
+    matic: {
+      url: "https://chaotic-dawn-patina.matic-testnet.discover.quiknode.pro/c58e5724a4aa00a37479140a4092d66d83321c98/",
+      accounts: ["c58e5724a4aa00a37479140a4092d66d83321c98"]
+    }
+  },
+  solidity: "0.8.17"
 };
